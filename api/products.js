86501389039b63cb8b3e5c6081fe904e30457772
Shiza Javaid <shizajavaid198l@gmail.com>
2026-08-14 17:12:53 +0500

    feat: add root-level /api/products serverless function

---
+++ api/products.js
@@ -0,0 +1,18 @@
+const products = require('./full-stack-website/backend/products');
+
+module.exports = (req, res) => {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(204).end();
+  }
+
+  if (req.method === 'GET') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).json(products);
+  }
+
+  return res.status(405).json({ message: 'Method not allowed' });
+};
